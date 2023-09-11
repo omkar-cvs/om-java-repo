b084f10c5b5c22be4f4a0bde3e0e5819f3ac1e16
Omkar Singh <omkarsinghaws@gmail.com>
2023-09-11 23:54:17 +0530

    BaseCode
    Base Code added for spring boot

---
+++ web-app-all-be/src/main/java/com/oks/web/app/all/be/WebAppAllBeApplication.java
@@ -0,0 +1,13 @@
+package com.oks.web.app.all.be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAppAllBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebAppAllBeApplication.class, args);
+	}
+
+}
---
+++ web-app-all-be/src/test/java/com/oks/web/app/all/be/WebAppAllBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oks.web.app.all.be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebAppAllBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
